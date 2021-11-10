47e7799c62f535010c1610503927fdd3d7926e3d
Rahel Muhammad <rahelmuhammad@gmail.com>
2021-11-10 23:42:50 +0300

    react app

---
+++ client/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = (props) => {
+    return (
+        <div>
+            <h1 className='mt-10 text-center text-xl animate-bounce'>
+                React App + TailwindCSS served by ExpressJS! 🔥
+            </h1>
+        </div>
+    );
+};
+
+export default App;

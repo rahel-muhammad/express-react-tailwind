48eed8789a469a4fcb2eaa65a79b1b79fa0f96c7
Rahel Muhammad <rahelmuhammad@gmail.com>
2021-11-10 23:56:12 +0300

    h1 style improvements

--- client/App.js
+++ client/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 const App = (props) => {
     return (
         <div>
-            <h1 className='mt-10 text-center text-xl animate-bounce'>
+            <h1 className='mt-16 text-center text-3xl animate-bounce'>
                 React App + TailwindCSS served by ExpressJS! 🔥
             </h1>
         </div>

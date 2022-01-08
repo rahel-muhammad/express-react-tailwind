003d5a6dd845d3de4f346c948395909e96ab79da
Rahel Muhammad <rahelmuhammad@gmail.com>
2022-01-08 11:02:11 +0300

    updated with tailwind 3 and CRA 5

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
     return (
         <div className='text-center'>
-            <h1 className='text-xl mt-4'>React + TailwindCSS</h1>
+            <h1 className='text-3xl mt-4'>React + TailwindCSS</h1>
         </div>
     );
 }
